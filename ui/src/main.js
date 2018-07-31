0969b704d296b146e36cfac3c4e8b0c7580e4c74
Eduardo Schettino <schettino72@gmail.com>
2018-07-31 16:59:45 +0800

    ui: mount vue on template without a root component.

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+Vue.component('hello', HelloWorld)
+
+new Vue({el: '#app'})
+

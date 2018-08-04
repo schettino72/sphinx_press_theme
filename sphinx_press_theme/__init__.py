f032ed2f48f6a8e94203c3ce457b14f9ad6ec0f3
Eduardo Schettino <schettino72@gmail.com>
2018-08-05 04:01:30 +0800

    add navlinks from toctrees included in master doc.

--- sphinx_press_theme/__init__.py
+++ sphinx_press_theme/__init__.py
@@ -1,5 +1,34 @@
 from os import path
 
+from sphinx import addnodes
+
+
+def add_doctree_data(app, pagename, templatename, context, doctree):
+    # FIXME how to cache this, so it is not called for every page?
+    master = app.env.get_doctree(app.env.config.master_doc)
+    res = []
+    for tree in master.traverse(addnodes.toctree):
+        entries = []
+        current0 = False
+        for title, name in tree['entries']:
+            current1 = (pagename == name)
+            if current1:
+                current0 = True
+            if not title:
+                title = app.env.titles[name].astext()
+            entries.append({
+                'name': name,
+                'title': title,
+                'current': current1,
+            })
+        res.append({
+            'title': tree['caption'],
+            'current': current0,
+            'entries': entries,
+        })
+    context['toctree_data'] = res
+
+
 def setup(app):
-    # app.add_builder(builder,override=True)
+    app.connect('html-page-context', add_doctree_data)
     app.add_html_theme('press', path.abspath(path.dirname(__file__)))

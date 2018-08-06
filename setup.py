fcb203e84e9611c5dcb620c72b0a1d396aa94e10
Eduardo Schettino <schettino72@gmail.com>
2018-08-07 03:41:59 +0800

    release 0.1

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='sphinx_press_theme',
-    version='0.1.0',
-    #url=
+    version='0.1.1',
+    url='https://schettino72.github.io/sphinx_press_site/',
     license='MIT',
     author='Eduardo Naufel Schettino <schetino72>',
-    # description
-    # long_description
+    description='A Sphinx-doc theme based on Vuepress',
+    long_description = long_description,
     packages=['sphinx_press_theme'],
     package_data={'sphinx_press_theme': [
         'theme.conf',
         '*.html',
-        'static/css/*.css',
-        'static/js/*.js',
-        'static/font/*.*'
+        'util/*.html',
+        'static/*.css',
+        'static/*.js',
     ]},
         entry_points = {
         'sphinx.html_themes': [
@@ -27,7 +30,7 @@ setup(
     classifiers=[
         'Framework :: Sphinx',
         'Framework :: Sphinx :: Theme',
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Environment :: Console',
         'Environment :: Web Environment',
@@ -40,4 +43,10 @@ setup(
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
     ],
+    keywords = "sphinx doc theme vue.js",
+    project_urls = {
+        'Documentation': 'https://schettino72.github.io/sphinx_press_site/',
+        'Source': 'https://schettino72.github.io/sphinx_press_site/',
+        'Tracker': 'https://schettino72.github.io/sphinx_press_site/issues',
+    },
 )

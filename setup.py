309fa91462724c3c15eab740e0bb0c891dba423c
Zulko <valentin.zulkower@gmail.com>
2019-10-01 22:03:02 +0100

    small setup.py fixes for PyPI

--- setup.py
+++ setup.py
@@ -1,52 +1,49 @@
 from setuptools import setup
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='sphinx_press_theme',
-    version='0.5.0',
-    url='https://schettino72.github.io/sphinx_press_site/',
-    license='MIT',
-    author='Eduardo Naufel Schettino <schetino72>',
-    description='A Sphinx-doc theme based on Vuepress',
-    long_description = long_description,
-    packages=['sphinx_press_theme'],
-    package_data={'sphinx_press_theme': [
-        'theme.conf',
-        '*.html',
-        'util/*.html',
-        'static/*.css',
-        'static/*.js',
-    ]},
-        entry_points = {
-        'sphinx.html_themes': [
-            'press = sphinx_press_theme',
+    name="sphinx_press_theme",
+    version="0.5.1",
+    url="https://schettino72.github.io/sphinx_press_site/",
+    license="MIT",
+    author="Eduardo Naufel Schettino <schetino72>",
+    description="A Sphinx-doc theme based on Vuepress",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["sphinx_press_theme"],
+    package_data={
+        "sphinx_press_theme": [
+            "theme.conf",
+            "*.html",
+            "util/*.html",
+            "static/*.css",
+            "static/*.js",
         ]
     },
-    install_requires=[
-       'sphinx>=2.0.0'
-    ],
+    entry_points={"sphinx.html_themes": ["press = sphinx_press_theme"]},
+    install_requires=["sphinx>=2.0.0"],
     classifiers=[
-        'Framework :: Sphinx',
-        'Framework :: Sphinx :: Theme',
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Operating System :: OS Independent',
-        'Topic :: Documentation',
-        'Topic :: Software Development :: Documentation',
+        "Framework :: Sphinx",
+        "Framework :: Sphinx :: Theme",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
     ],
-    keywords = "sphinx doc theme vue.js",
-    project_urls = {
-        'Documentation': 'https://schettino72.github.io/sphinx_press_site/',
-        'Source': 'https://schettino72.github.io/sphinx_press_site/',
-        'Tracker': 'https://schettino72.github.io/sphinx_press_site/issues',
+    keywords="sphinx doc theme vue.js",
+    project_urls={
+        "Documentation": "https://schettino72.github.io/sphinx_press_site/",
+        "Source": "https://github.com/schettino72/sphinx_press_theme",
+        "Tracker": "https://schettino72.github.io/sphinx_press_site/issues",
     },
 )

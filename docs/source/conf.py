83e343feef2bbba69f410170fc83e8185d3495c9
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-03-22 21:30:05 -0400

    Update release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Eduardo Naufel Schettino'
 # The short X.Y version
 version = '0.9'
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.9.1'
 
 
 # -- General configuration ---------------------------------------------------
